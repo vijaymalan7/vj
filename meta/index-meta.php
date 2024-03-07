4f4237c779ee1c02f84489bbcbca342578ae896b
Vijay <vijay@matrixinfocom.com>
2024-03-07 14:17:55 +0530

    add files

---
+++ meta/index-meta.php
@@ -0,0 +1,19 @@
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta property="og:title" content="Mr. Vijay Malan: Software Engineer &amp; Developer">
+  <meta name="og:description"
+    content="Hello this is Vijay Malan, I'm Software Engineer, Provide clean Website, Android and IOS app and many more.">
+  <meta property="og:image" content="https://vijaymalan7.github.io/vijay/t2%20-%20Copy.jpg">
+  <meta name="keywords"
+    content=" vijay malan,portfolio,html,css,python,mysql,JavaScript,how to,website,best protfolio website,portfolio best template,machine learning ">
+  <meta property="og:url" content="https://vijaymalan7.github.io/vijay">
+  <meta property="og:type" content="website">
+  <meta property="og:locale" content="en_US">
+  <meta property="og:site_name" content="Mr. Vijay Malan">
+  <meta property="article:author" content="Mr. Vijay Malan">
+  <meta property="article:section" content="Portfolio">
+  <meta property="article:tag" content="Software Engineer, Developer, & Business solution">
+
+
+<!-- fav icon -->
+<link rel="shortcut icon" href="img/logo/logo.jpg" type="image/x-icon">
